8961ec8f4f5bed8fbe00461789af53a71e4f187c
Uche Nweke Jr <unwekejr@gmail.com>
2020-06-06 00:22:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+if array.size == 1 
+  array.join
+elsif array.size == 2
+array.join(" and ")
+elsif array.size > 2
+  last_word = array.pop
+  array.join(", ") + ", and #{last_word}"
 
+end
 end
